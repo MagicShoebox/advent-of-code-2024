bb1023d0d948ccf2d5d90b0390e1033d7d577b0e
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2024-12-17 22:44:20 -0500

    Day 1 part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,40 @@
-use crate::SolveResult;
+use std::iter;
 
-pub fn solve(_input: &str) -> SolveResult {
-    Ok((String::new(), String::new()))
+use crate::{SolveError, SolveResult};
+
+pub fn solve(input: &str) -> SolveResult {
+    let mut cols = parse(input)?;
+    Ok((part1(&mut cols), part2(&mut cols)))
+}
+
+fn parse(input: &str) -> Result<[Vec<u32>; 2], SolveError> {
+    let mut cols: Vec<Vec<u32>> = vec![];
+    for line in input.lines() {
+        for (i, num) in line.split_whitespace().enumerate() {
+            let mut col = cols.get_mut(i);
+            while let None = col {
+                cols.push(vec![]);
+                col = cols.get_mut(i);
+            }
+            col.unwrap().push(num.parse()?);
+        }
+    }
+    match (cols.pop(), cols.pop()) {
+        (Some(x), Some(y)) => Ok([x, y]),
+        _ => Err("Input did not have 2 columns.".into()),
+    }
+}
+
+fn part1(cols: &mut [Vec<u32>; 2]) -> String {
+    cols[0].sort();
+    cols[1].sort();
+    let diff_sum: u32 = iter::zip(&cols[0], &cols[1])
+        .map(|(a, b)| a.abs_diff(*b))
+        .sum();
+
+    diff_sum.to_string()
+}
+
+fn part2(cols: &mut [Vec<u32>; 2]) -> String {
+    String::new()
 }
