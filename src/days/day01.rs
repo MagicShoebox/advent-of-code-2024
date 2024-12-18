71e3d365e991e7810357f4dbb9217ff3e63b691f
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2024-12-17 23:17:11 -0500

    Day 1 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,13 +1,14 @@
-use std::iter;
-
 use crate::{SolveError, SolveResult};
+use std::{collections::HashMap, iter};
+
+type Columns = (Vec<u32>, Vec<u32>);
 
 pub fn solve(input: &str) -> SolveResult {
     let mut cols = parse(input)?;
-    Ok((part1(&mut cols), part2(&mut cols)))
+    Ok((part1(&mut cols), part2(cols)))
 }
 
-fn parse(input: &str) -> Result<[Vec<u32>; 2], SolveError> {
+fn parse(input: &str) -> Result<Columns, SolveError> {
     let mut cols: Vec<Vec<u32>> = vec![];
     for line in input.lines() {
         for (i, num) in line.split_whitespace().enumerate() {
@@ -20,21 +21,28 @@ fn parse(input: &str) -> Result<[Vec<u32>; 2], SolveError> {
         }
     }
     match (cols.pop(), cols.pop()) {
-        (Some(x), Some(y)) => Ok([x, y]),
+        (Some(x), Some(y)) => Ok((x, y)),
         _ => Err("Input did not have 2 columns.".into()),
     }
 }
 
-fn part1(cols: &mut [Vec<u32>; 2]) -> String {
-    cols[0].sort();
-    cols[1].sort();
-    let diff_sum: u32 = iter::zip(&cols[0], &cols[1])
+fn part1(cols: &mut Columns) -> String {
+    cols.0.sort();
+    cols.1.sort();
+    iter::zip(&cols.0, &cols.1)
         .map(|(a, b)| a.abs_diff(*b))
-        .sum();
-
-    diff_sum.to_string()
+        .sum::<u32>() // my first Turbofish
+        .to_string()
 }
 
-fn part2(cols: &mut [Vec<u32>; 2]) -> String {
-    String::new()
+fn part2(cols: Columns) -> String {
+    let mut counter: HashMap<u32, u32> = HashMap::new();
+    for y in cols.1 {
+        *counter.entry(y).or_insert(0) += 1;
+    }
+    cols.0
+        .into_iter()
+        .map(|x| x * counter.get(&x).unwrap_or(&0))
+        .sum::<u32>()
+        .to_string()
 }
