54cf6eb1cc36c7ea6b0585cecac4ee28ced9520a
Daniel Schoenbach <10995699+MagicShoebox@users.noreply.github.com>
2025-08-18 23:40:07 -0400

    Day 6 part 2 (incorrect)

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::{BTreeSet, HashMap, HashSet},
     ops::Bound::{Excluded, Unbounded},
 };
 
@@ -12,6 +12,7 @@ struct Grid {
     size: (usize, usize),
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     North,
     East,
@@ -31,8 +32,9 @@ impl Direction {
 }
 
 pub fn solve(input: &str) -> SolveResult {
-    let grid = parse(input);
-    Ok((part1(&grid), String::new()))
+    let mut grid = parse(input);
+    let vertices = calc_vertices(&grid);
+    Ok((part1(&grid, &vertices), part2(&mut grid, &vertices)))
 }
 
 fn parse(input: &str) -> Grid {
@@ -59,7 +61,8 @@ fn parse(input: &str) -> Grid {
     grid
 }
 
-fn part1(grid: &Grid) -> String {
+fn calc_vertices(grid: &Grid) -> Vec<(usize, usize)>
+{
     let mut vertices = vec![grid.start];
     let mut pos = grid.start;
     let mut dir = Direction::North;
@@ -76,8 +79,7 @@ fn part1(grid: &Grid) -> String {
             }
         }
     }
-
-    steps(grid, &vertices).to_string()
+    vertices
 }
 
 fn calc_dest(grid: &Grid, pos: &(usize, usize), dir: &Direction) -> Option<(usize, usize)> {
@@ -130,99 +132,73 @@ fn final_dest(grid: &Grid, pos: &(usize, usize), dir: &Direction) -> (usize, usi
     }
 }
 
-fn steps(grid: &Grid, vertices: &[(usize, usize)]) -> usize {
-    let mut buffer: Vec<Vec<bool>> = (0..grid.size.0)
-        .map(|_| (0..grid.size.1).map(|_| false).collect())
-        .collect();
-    for (i, (p1, p2)) in vertices.iter().zip(vertices[1..].iter()).enumerate() {
-        let r = match i % 4 {
-            0 => p2.0..=p1.0,
-            1 => p1.1..=p2.1,
-            2 => p1.0..=p2.0,
-            3 => p2.1..=p1.1,
-            _ => panic!(),
+fn part1(grid: &Grid, vertices: &[(usize, usize)]) -> String {
+    let mut buffer = vec![vec![false; grid.size.1]; grid.size.0];
+    let mut dir = Direction::North;
+    for (p1, p2) in vertices.iter().zip(vertices[1..].iter()) {
+        let r = match dir {
+            Direction::North => p2.0..=p1.0,
+            Direction::East => p1.1..=p2.1,
+            Direction::South => p1.0..=p2.0,
+            Direction::West => p2.1..=p1.1,
         };
         for x in r {
-            match i % 4 {
-                0 => buffer[x][p1.1] = true,
-                1 => buffer[p1.0][x] = true,
-                2 => buffer[x][p1.1] = true,
-                3 => buffer[p1.0][x] = true,
-                _ => panic!(),
+            match dir {
+                Direction::North | Direction::South => buffer[x][p1.1] = true,
+                Direction::East | Direction::West => buffer[p1.0][x] = true,
             }
         }
+        dir = dir.turn();
     }
-    buffer.iter().flatten().filter(|&x| *x).count()
+    buffer.iter().flatten().filter(|&x| *x).count().to_string()
 }
 
-
-/*
-
-* I wanted to find a way to calculate the steps without
-* storing each step individually in an array or set
-* (thus allowing for grids of arbitrary size).
-
-* Unfortunately, accounting for the intersections and overlaps
-* was more difficult than I expected.
-* This is the last version I had before I abandoned the approach
-* and used the method above in order to continue AoC.
-
-fn steps_arbitrary_size(vertices: &[(usize, usize)]) -> usize {
-    let mut horiz: BTreeMap<usize, Vec<RangeInclusive<usize>>> = BTreeMap::new();
-    let mut verts: BTreeMap<usize, Vec<RangeInclusive<usize>>> = BTreeMap::new();
-    let mut s: usize = 0; // initial position
-    for (i, (p1, p2)) in vertices.iter().zip(vertices[1..].iter()).enumerate() {
-        if i % 2 == 0 {
-            let col = p1.1;
-            let range = if i % 4 == 0 { p2.0..=p1.0 } else { p1.0..=p2.0 };
-            let dist = p1.0.abs_diff(p2.0) + 1;
-            s += dist;
-
-            s -= verts
-                .get(&col)
-                .map_or(0, |v| v.iter().map(|r| overlap(&range, r)).sum());
-
-            s -= horiz
-                .range(range.clone())
-                .flat_map(|(_, v)| v)
-                .filter(|r| r.contains(&col))
-                .count();
-
-            if dist > 1 {
-                verts.entry(col).or_default().push(range);
+fn part2(grid: &mut Grid, vertices: &[(usize, usize)]) -> String {
+    let mut loop_count = 0;
+    let mut dir = Direction::North;
+    for (p1, p2) in vertices.iter().zip(vertices[1..].iter()) {
+        let r = match dir {
+            Direction::North => p2.0+1..p1.0+1,
+            Direction::East => p1.1..p2.1,
+            Direction::South => p1.0..p2.0,
+            Direction::West => p2.1+1..p1.1+1,
+        };
+        for x in r {
+            let (pos, block) = match dir {
+                Direction::North => ((x, p1.1), (x - 1, p1.1)),
+                Direction::South => ((x, p1.1), (x + 1, p1.1)),
+                Direction::East => ((p1.0, x), (p1.0, x + 1)),
+                Direction::West => ((p1.0, x), (p1.0, x - 1)),
+            };
+            if block == grid.start {
+                continue;
             }
-        } else {
-            let row = p1.0;
-            let range = if i % 4 == 1 { p1.1..=p2.1 } else { p2.1..=p1.1 };
-            let dist = p1.1.abs_diff(p2.1) + 1;
-            s += dist;
-
-            s -= horiz
-                .get(&row)
-                .map_or(0, |v| v.iter().map(|r| overlap(&range, r)).sum());
-
-            s -= verts
-                .range(range.clone())
-                .flat_map(|(_, v)| v)
-                .filter(|r| r.contains(&row))
-                .count();
-
-            if dist > 1 {
-                horiz.entry(row).or_default().push(range);
+            grid.rows.entry(block.0).or_default().insert(block.1);
+            grid.cols.entry(block.1).or_default().insert(block.0);
+            if check_loop(grid, pos, dir) {
+                loop_count += 1;
             }
+            grid.rows.get_mut(&block.0).map(|v| v.remove(&block.1));
+            grid.cols.get_mut(&block.1).map(|v| v.remove(&block.0));
+        }
+        dir = dir.turn();
+    }
+    loop_count.to_string()
+}
+
+fn check_loop(grid: &Grid, mut pos: (usize, usize), mut dir: Direction) -> bool {
+    let mut loop_set = HashSet::new();
+    loop {
+        match calc_dest(grid, &pos, &dir) {
+            Some(dest) if loop_set.contains(&(dest, dir)) => {
+                return true
+            },
+            Some(dest) => {
+                loop_set.insert((dest, dir));
+                pos = dest;
+                dir = dir.turn();
+            }
+            None => return false
         }
     }
-    s
 }
-
-fn overlap(r1: &RangeInclusive<usize>, r2: &RangeInclusive<usize>) -> usize {
-    if r1.start() > r2.start() {
-        return overlap(r2, r1);
-    }
-    if r1.end() < r2.start() {
-        return 0;
-    }
-    min(r1.end(), r2.end()) - r2.start() + 1
-}
-
-*/
